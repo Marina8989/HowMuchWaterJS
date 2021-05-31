dac8e2063b8e7a95b3e80de4a322db83e150a26e
Marina8989 <marina.mill209@gmail.com>
2021-05-30 18:22:38 -0700

    added image

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function addToBigCup() {
     
     if(fullCups == 0) {
         percentage.style.visibility = 'hidden';
-        percentage.style.height = 0;
+        percentage.style.height = 0; 
     }else {
         percentage.style.visibility = 'visible';
         percentage.style.height = `${fullCups/totalCups * 320}px`;
